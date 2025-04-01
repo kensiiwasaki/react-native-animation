6da0501b40255286552992b6aeace8d1e2ca7400
kensiiwasaki <kensiiwasaki@gmail.com>
2025-04-02 04:08:58 +0900

    Update metro.config.js to include minifier configuration

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = {
+  ...config,
+  transformer: {
+    ...config.transformer,
+    minifierConfig: {
+      compress: {
+        passes: 2,
+      },
+    },
+  },
+};
